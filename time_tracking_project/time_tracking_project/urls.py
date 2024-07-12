eeeac2c3c26823d8e3ec3c79eec735a46f426e40
Stepan Lazoryk <lazoriksua@gmail.com>
2024-07-12 23:39:46 +0100

    delete test project, add new my project, create database in postgresql, migrate

--- time_tracking_project/time_tracking_project/urls.py
+++ time_tracking_project/time_tracking_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for time_tracking_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,10 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from hello_world import views as index_views
+from django.urls import path
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- time_tracking_project/tracking/apps.py
+++ time_tracking_project/tracking/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloWorldConfig(AppConfig):
+class TrackingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello_world'
+    name = 'tracking'
