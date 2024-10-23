c94f4d1bcd470e743d9e5fe7792d4154f9d3f998
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-10-23 17:22:29 +0100

    try fix all

--- comeincomeout/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('report/', views.report_view, name='report'),
-    path('register_time/', views.register_time_view, name='register_time'),
-]
