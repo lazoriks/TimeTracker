6381e4e819da0107a30f8859b0ba7a1a3c680cad
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-10-24 00:53:34 +0100

    change model user

--- core/models.py
+++ core/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    user = models.CharField(max_length=150, unique=True)
     name = models.CharField(max_length=250)
     surname = models.CharField(max_length=250)
-    password = models.CharField(max_length=50)
     super_user = models.BooleanField(default=False)
-    email = models.EmailField(max_length=150, blank=True, null=True)
+    email = models.EmailField(max_length=150, blank=True, null=True) 
     mobile = models.CharField(max_length=15, blank=True, null=True)
 
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
     def __str__(self):
-        return f"{self.name} {self.surname} ({self.user})"
+        return f"{self.name} {self.surname} ({self.username})"
 
 class Hours(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
