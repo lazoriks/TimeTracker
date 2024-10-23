22ed1593c7049634c4ddbc791147f55c28a1bf9b
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-10-24 00:23:51 +0100

    Redirect to login page

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import login_view, user_list_view, last_time_view, report_view
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('user_list/', user_list_view, name='user_list'),
     path('last_time/', last_time_view, name='last_time'),
     path('report/', report_view, name='report'),
+    path('', lambda request: redirect('login')),  # Redirect to login page
 ]
