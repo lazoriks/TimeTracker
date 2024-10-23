62490428e322f8b08ad92582ef106eaa2e2a0d39
Stepan <31982882+lazoriks@users.noreply.github.com>
2024-10-24 00:58:58 +0100

    user grops and permision

--- core/models.py
+++ core/models.py
@@ -8,6 +8,22 @@ class User(AbstractUser):
     email = models.EmailField(max_length=150, blank=True, null=True) 
     mobile = models.CharField(max_length=15, blank=True, null=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',  
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',  
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
+
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
